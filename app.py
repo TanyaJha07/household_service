820525a18f12b474d0285a5b896d3d18ede33c60
Tanya Jha <tanyajhaanni@gmail.com.com>
2024-11-23 09:16:29 +0530

    err

--- app.py
+++ app.py
@@ -135,6 +135,13 @@ def signup_professional():
 
     return render_template("signup_professional.html")
 
+def create_admin():
+    admin = User(username="admin", password="admin123", role="admin")
+    db.session.add(admin)
+    db.session.commit()
+
+
+
 # Define a route for login
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -334,4 +341,6 @@ def logout():
 
 # Run the app
 if __name__ == "__main__":
+    if  User.query.filter_by(username="admin").first() is None:
+        create_admin()
     app.run(debug=True)
